a2b0ddfc0321eebd6d745f9a60893942473d1fbd
Joselin17 <59721527+Joselin17@users.noreply.github.com>
2021-03-05 08:41:36 -0800

    Update index.php

--- src/auto/index.php
+++ src/auto/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Autocomplete using PHP/MySQL and jQuery</title>
+<title>Online Book Store</title>
 <link rel="stylesheet" href="css/style.css" />
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/script.js"></script>
